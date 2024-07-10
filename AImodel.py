7f548043b106c97b20e971b1acdca94fa8a08e56
thekeensoul <ribhu.s18@gmail.com>
2024-07-10 11:51:21 +0530

    pushing both files to AI model branch

--- AImodel.py
+++ AImodel.py
@@ -1,1 +1,7 @@
 print("adding all ML related code here")
+
+model1 = 'result acc 45'
+model2 = ' result acc 60'
+model3 = 'res acc 78'
+
+print('model 3 is best')
--- dataPros.py
+++ dataPros.py
@@ -1,2 +1,8 @@
 
 print("adding all dpro files")
+
+model1 = 'result acc 45'
+model2 = ' result acc 60'
+model3 = 'res acc 78'
+
+print('model 3 is best')
